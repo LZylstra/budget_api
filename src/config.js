00a601fdc8829919c218e8822e9f6fd33fb2fb29
Lindsey Zylstra <lindseyzylstra@gmail.com>
2020-10-28 14:17:49 -0700

    adds middleware for error handling, auth, logging, updates to server and config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL:
+    process.env.DATABASE_URL,
+  CLIENT_ID: process.env.CLIENT_ID,
+  JWT_SECRET: process.env.JWT_SECRET || "purplecow",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "15m"
+
   }
